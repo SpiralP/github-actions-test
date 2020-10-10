b73af3548c1acbb14c2c88f6d1efd81d3a928f31
SpiralP <spiral.palmer@gmail.com>
2020-10-09 17:57:38 -0700

    a

--- .github/format-release.js
+++ .github/format-release.js
@@ -1,11 +1,19 @@
 const analysis = process.argv[2];
-
-console.log(
-  analysis
-    .split(",")
-    .map((line) => {
-      const [file, url] = line.split("=");
-      return `\`${file}\`: [VirusTotal](${url})`;
-    })
-    .join("\n")
+const version = process.env.GITHUB_REF.replace(/^refs\/tags\//, "").replace(
+  /^v/,
+  ""
 );
+
+const lines = [`Release version ${version}`, ""];
+
+analysis
+  .split(",")
+  .map((line) => {
+    const [file, url] = line.split("=");
+    return `\`${file}\`: [VirusTotal analysis](${url})`;
+  })
+  .forEach((line) => {
+    lines.push(line);
+  });
+
+console.log(lines.join("\n"));
